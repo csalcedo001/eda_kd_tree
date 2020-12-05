2e042852682b5ae9b81d4e480fe87b4663f1f0b6
Guillermo598 <agfranco20@gmail.com>
2020-12-05 16:35:22 -0500

    haar test 3

--- app/rgb_mariposas.cpp
+++ app/rgb_mariposas.cpp
@@ -9,22 +9,20 @@ using namespace cimg_library;
 using namespace std;
 
 vector<double> vectorize(CImg<double> & img) {
-    CImg<double> a, b, c, d;
-    a = b = c = d = img.haar(false, 2);
+    CImg<double> a;
+    a = img.haar(false, 2);
 
-    a = a.crop(0,0,75,51);
-    b = b.crop(76,0,151,51);
-    c = c.crop(0,52,75,103);
-    d = d.crop(76,52,151,103);
+    a = a.crop(0,0,63,63);
 
     vector<double> result;
-    cimg_forXY(a, x, y) { result.push_back((a(x,y,0)+a(x,y,1)+a(x,y,2))/3); }
-    cimg_forXY(b, x, y) { result.push_back((b(x,y,0)+b(x,y,1)+b(x,y,2))/3); }
-    cimg_forXY(c, x, y) { result.push_back((c(x,y,0)+c(x,y,1)+c(x,y,2))/3); }
-    cimg_forXY(d, x, y) { result.push_back((d(x,y,0)+d(x,y,1)+d(x,y,2))/3); }
+    cimg_forXY(a, x, y) { 
+        result.push_back(a(x,y,0));
+        result.push_back(a(x,y,1));
+        result.push_back(a(x,y,2));
+    }
 
     return result;
-}
+
 
 
 vector<vector<double>> get_vectors(string directory_name, vector<string> &classes) {
@@ -35,7 +33,7 @@ vector<vector<double>> get_vectors(string directory_name, vector<string> &classe
 		entry_path.append("/");
 		entry_path.append(entry_s);
 		CImg<double> A(entry_path.c_str());
-		A.resize(304,208);
+		A.resize(128,128);
 		vector<double> vA = vectorize(A);
 		points.push_back(vA); 
 		classes.push_back(entry_s.substr(0,3));
